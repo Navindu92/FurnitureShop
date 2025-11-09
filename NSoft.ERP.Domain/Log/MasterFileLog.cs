874de2a446bf61cd94650bbba7892198b65f6a0b
user <user@DESKTOP-BQ8S25R>
2025-11-09 20:04:08 +0530

    add miss files

---
+++ NSoft.ERP.Domain/Log/MasterFileLog.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace NSoft.ERP.Domain.Log
+{
+    public class MasterFileLog
+    {
+        public long MasterFileLogID { get; set; }
+        [MaxLength(20)]
+        public string FormName { get; set; }
+        [MaxLength(20)]
+        public string Username { get; set; }
+        public string PCName { get; set; }
+        [MaxLength(20)]
+        public string Location { get; set; }
+        [MaxLength(10)]
+        public string Action { get; set; }
+        public DateTime LogDate { get; set; }
+        public TimeSpan LogTime { get; set; }
+
+    }
+}
---
+++ NSoft.ERP.Domain/Log/SystemLog.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace NSoft.ERP.Domain.Log
+{
+    public class SystemLog
+    {
+        public long SystemLogID { get; set; }
+       
+        [MaxLength(20)]
+        public string Username { get; set; }
+        [MaxLength(20)]
+        public string Location { get; set; }
+        public string MachineName { get; set; }
+        public string DomainName { get; set; }
+        public string PCUserName { get; set; }
+        public string OsVersion { get; set; }
+        public DateTime LoggedDate { get; set; }
+        public TimeSpan LoggedTime { get; set; }
+        public DateTime LogoutDate { get; set; }
+        public TimeSpan LogoutTime { get; set; }
+    }
+}
